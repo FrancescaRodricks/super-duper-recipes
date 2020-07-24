9ad8139c43726cf0e8f4e1280592c4ba8369f4d0
Francesca Rodricks <francesca.rodricks@simplybusiness.co.uk>
2020-07-24 18:51:19 +0100

    Updates

---
+++ precache-manifest.01bd5e36747071c989b42cbc19aca1aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9122016d29137828001a35a614c1a4b0",
+    "url": "/super-duper-recipes/index.html"
+  },
+  {
+    "revision": "d8c01cb19786aaee721b",
+    "url": "/super-duper-recipes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a901f506977c6419868d",
+    "url": "/super-duper-recipes/static/js/2.8a9d8d22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/super-duper-recipes/static/js/2.8a9d8d22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8c01cb19786aaee721b",
+    "url": "/super-duper-recipes/static/js/main.070def4c.chunk.js"
+  },
+  {
+    "revision": "c588dd9541b603d468f5",
+    "url": "/super-duper-recipes/static/js/runtime-main.c7d5d398.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/super-duper-recipes/static/media/logo.5d5d9eef.svg"
+  }
+]);
