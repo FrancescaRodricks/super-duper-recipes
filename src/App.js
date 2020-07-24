adc8ba6c2cdb44a3cc1c710ff35bdae70182c585
Francesca Rodricks <francesca.rodricks@simplybusiness.co.uk>
2020-07-24 19:22:12 +0100

    adds router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header>
-        <p>
-          TODO : Nothing here yet :P
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+};
+
+const Home = () => {
+  return <h2>Home</h2>;
+}
+
+const About = () => {
+  return <h2>About</h2>;
 }
 
 export default App;
